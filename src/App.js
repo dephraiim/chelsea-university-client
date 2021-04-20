7ef2f470200bb657beae6923994919db1047a12d
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-04-20 07:03:24 +0000

    signup and login pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
 import Content from './components/Content';
-import Header from './components/Header';
+import Login from './components/Login';
+import Protected from './components/Protected';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className='h-screen bg-indigo-100'>
-      <Header />
-      <Content />
-    </div>
+    <Router>
+      <div className='h-screen bg-indigo-100'>
+        <div>
+          <div className='header-2'>
+            <nav className='bg-white py-2 md:py-4'>
+              <div className='container px-4 mx-auto md:flex md:items-center'>
+                <div className='flex justify-between items-center'>
+                  <Link to='/'>
+                    <a className='font-bold text-xl text-indigo-600'>
+                      Chelsea University
+                    </a>
+                  </Link>
+                </div>
+
+                <div className='hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0'>
+                  <Link to='/protected'>
+                    <a className='p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-transparent rounded hover:bg-indigo-600 hover:text-white transition-colors duration-300'>
+                      Protected
+                    </a>
+                  </Link>
+                  <Link to='/login'>
+                    <a className='p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-transparent rounded hover:bg-indigo-600 hover:text-white transition-colors duration-300'>
+                      Login
+                    </a>
+                  </Link>
+                  <Link to='/register'>
+                    <a className='p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-solid border-indigo-600 rounded hover:bg-indigo-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1'>
+                      Signup
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <Switch>
+          <Route exact path='/'>
+            <Content />
+          </Route>
+          <Route path='/protected'>
+            <Protected />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+export default function Login() {
+  return (
+    <div>
+      <div class='flex h-screen bg-indigo-700'>
+        <div class='w-full max-w-xs m-auto bg-indigo-100 rounded p-5'>
+          <header>
+            <img
+              alt='logo'
+              class='w-20 mx-auto mb-5'
+              src='https://img.icons8.com/fluent/344/year-of-tiger.png'
+            />
+          </header>
+          <form>
+            <div>
+              <label class='block mb-2 text-indigo-500' for='email'>
+                Email
+              </label>
+              <input
+                class='w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300'
+                type='text'
+                name='email'
+              />
+            </div>
+            <div>
+              <label class='block mb-2 text-indigo-500' for='password'>
+                Password
+              </label>
+              <input
+                class='w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300'
+                type='password'
+                name='password'
+              />
+            </div>
+            <div>
+              <input
+                class='w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded'
+                type='submit'
+              />
+            </div>
+          </form>
+          <footer>
+            <a
+              href='/register'
+              class='text-indigo-700 hover:text-pink-700 text-sm float-right'
+            >
+              Create Account
+            </a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
